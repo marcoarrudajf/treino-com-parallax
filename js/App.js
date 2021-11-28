8e93493ed28f4172a4cce5c6e1f09915f19fe444
marcoarrudajf <marcoarruda.dev@gmail.com>
2021-11-28 11:51:53 -0300

    Criaçao de botão para retornar ao top

--- js/App.js
+++ js/App.js
@@ -2,6 +2,10 @@
 //função para mover o scroll da pagina através dos links href.
 function initScrollSuave() {
     const linksInternos = document.querySelectorAll('.ul-nav a[href^="#"]')
+    const header = document.querySelector('.cabecalho');
+    const btnVoltar = document.createElement('button');
+    header.appendChild(btnVoltar);
+
     if (linksInternos) {
         function scrolToSection(event) {
             event.preventDefault();
@@ -12,10 +16,22 @@ function initScrollSuave() {
                 block: 'start',
             })
         }
-
         linksInternos.forEach((link) => {
             link.addEventListener('click', scrolToSection)
         })
+
+        // botão scroll voltar para o topo.
+        btnVoltar.classList.toggle('btn-scroll')
+        btnVoltar.innerText = 'Voltar'
+        window.addEventListener("scroll", function () {
+            btnVoltar.classList.toggle("active", window.scrollY > 500);
+        });
+        if (btnVoltar) {
+            btnVoltar.addEventListener("click", () => {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            });
+        }
     }
 }
 initScrollSuave();
